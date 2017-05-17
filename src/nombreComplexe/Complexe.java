a4ed533f9639bd23c77891258e6ebe0b92c6df31
StraetenMax <maxstraeten@yahoo.com>
2017-05-17 16:03:46 +0200

    creation constructeurs

--- src/nombreComplexe/Complexe.java
+++ src/nombreComplexe/Complexe.java
@@ -1,5 +1,17 @@
 package nombreComplexe;
 
 public class Complexe {
-
+		//propriétés
+	private int partieReele;
+	private int partieImaginaire;
+	
+	//constructeurs vide initialisé
+	public Complexe(){
+		partieReele = 0;
+		partieImaginaire = 0;
+	}
+	public Complexe(int partieReele, int partieImaginaire){
+		this.partieReele = partieReele;
+		this.partieImaginaire = partieImaginaire;
+	}
 }
