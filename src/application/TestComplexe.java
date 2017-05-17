e3e1e9c7ef92f336f7a1b25d840f4d0634a69ffc
StraetenMax <maxstraeten@yahoo.com>
2017-05-17 17:58:14 +0200

    ajustement des appels

--- src/application/TestComplexe.java
+++ src/application/TestComplexe.java
@@ -11,9 +11,15 @@ public class TestComplexe {
 	
 		//appeler les méthodes
 		Complexe c3 = c1.plus(c2);
+		Complexe c4 = c1.moins(c2);
+		Complexe c5 = c3.plus(c4);
+		Complexe c6 = c3.moins(c4);
 		//System.out.println(c1.plus(c2));
 		//System.out.println(c1.moins(c2));
-		
+		System.out.println(c3);
+		System.out.println(c4);
+		System.out.println(c5);
+		System.out.println(c6);
 		
 
 		
